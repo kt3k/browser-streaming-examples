6a7cddcc8e719d6bfbf3f30c20a1b0d5418c7152
Yoshiya Hinosawa <stibium121@gmail.com>
2022-10-07 17:08:54 +0900

    init

---
+++ fetch2.mjs
@@ -0,0 +1,19 @@
+const resp = await fetch("https://dl.deno.land/canary/19e4e821d582b625797b8a17737dfa823a6aca21/deno-x86_64-apple-darwin.zip");
+ 
+const reader = resp.body.getReader();
+let i = 0;
+let total = 0;
+
+while (true) {
+  const { done, value } = await reader.read();
+
+  if (done) {
+    break;
+  }
+
+  const len = value.byteLength;
+  console.log(i, `${len}バイト、データ取得`);
+  total += len;
+  i++;
+}
+console.log(`合計${total}バイト`);
